8f1286f955c211f8d5d603827bdd22ed636fe5b3
TiciaChance <laticia.chance@flatironschool.com>
2016-09-19 10:24:38 -0400

    still problem solving the use of dynamic button

---
+++ Pods/Target Support Files/Pods-helloWorldSimpleApp/Pods-helloWorldSimpleApp-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_helloWorldSimpleAppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_helloWorldSimpleAppVersionString[];
+
--- helloWorldSimpleApp/ViewController.swift
+++ helloWorldSimpleApp/ViewController.swift
@@ -7,18 +7,35 @@
 //
 
 import UIKit
+import DynamicButton
+
 
 class ViewController: UIViewController {
+    
+    let dynamicButtonT = DynamicButton(style: DynamicButtonStyle.hamburger)
+
+
     @IBOutlet weak var welcomeButton: UIButton!
 
     @IBOutlet weak var greetingImage: UIImageView!
     
     @IBOutlet weak var backgroundImage: UIImageView!
     
+    @IBOutlet weak var firstDynamicButton: DynamicButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        firstDynamicButton = DynamicButton(style: DynamicButtonStyle.hamburger)
+
+        
+        // Animate the style update
+      //  firstDynamicButton.setStyle(DynamicButtonStyle.close, animated: true)
+
+        
     }
+    
 
     @IBAction func welcomeButtonTapped(_ sender: AnyObject) {
         
